80612a4a6b06b4668c5d3d90f440aebb0b45ba81
nittin20093 <nittin20093@iiitd.ac.in>
2021-12-25 14:53:59 +0530

    skeleton of all classes are created

--- The Will Hero Game/The Will Hero Game/src/application/CLASSES/Platform.java
+++ The Will Hero Game/The Will Hero Game/src/application/CLASSES/Platform.java
@@ -1,5 +1,9 @@
 package application.CLASSES;
 
-public class Platform {
+public class Platform extends Game_Objects {
+private int platform_length;
+public Platform(String col){
+    super(col);
+}
 
 }
